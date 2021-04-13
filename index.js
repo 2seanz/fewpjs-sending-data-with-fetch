dabb01a2371713358cd5aa8988f6b905df79f83f
Sean Ngo <seanzngo@gmail.com>
2021-04-13 14:19:31 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle"
+//     })
+// };
+// fetch( 'http://localhost:3000/dogs', configurationObject)
+//     .then(res => res.json())
+//     .then(function(object) {
+//         console.log(object);
+//     })
+
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// }); 
+
+function submitData(name, email)  {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name,
+        email
+    })
+})
+.then(res => res.json())
+.then( function(object) {
+    document.body.innerHTML = object["id"]
+})
+.catch(function (error) {
+    document.body.innerHTML = error.message
+})
+}
